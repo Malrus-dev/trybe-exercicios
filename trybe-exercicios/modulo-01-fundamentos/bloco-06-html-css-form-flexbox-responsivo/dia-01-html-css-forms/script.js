f2a2dd7a4cf3506d8aee0d312f44fb0fc7e03d20
Malrus-dev <maelsobfer@gmail.com>
2022-04-28 12:10:47 -0300

    exercicio finalizado

--- trybe-exercicios/modulo-01-fundamentos/bloco-06-html-css-form-flexbox-responsivo/dia-01-html-css-forms/script.js
+++ trybe-exercicios/modulo-01-fundamentos/bloco-06-html-css-form-flexbox-responsivo/dia-01-html-css-forms/script.js
@@ -5,26 +5,18 @@ window.onload = startPage;
 function startPage(){
     btnSbmtFtt.addEventListener("click", postForm);
     btnClearFtt.addEventListener("click", clearForm); 
-
-    var largura = window.innerWidth
-    || document.documentElement.clientWidth
-    || document.body.clientWidth;
-
-    var altura = window.innerHeight
-    || document.documentElement.clientHeight
-    || document.body.clientHeight;
-
-    console.log("altura", altura);
-    console.log("largura", largura);
-
 }
 function postForm(event){
     event.preventDefault();
-    console.log("fim")
 }
 function clearForm(){
     for(let i = 0; i < formsValues.length; i += 1){
-        console.log(formsValues[i]);
+        if (i > 1 && i < 6 || i > 7){
+            if (formsValues[i].checked){
+                formsValues[i].checked = false;
+            }            
+        }else{
+            formsValues[i].value = null;
+        }        
     }
-
 }
