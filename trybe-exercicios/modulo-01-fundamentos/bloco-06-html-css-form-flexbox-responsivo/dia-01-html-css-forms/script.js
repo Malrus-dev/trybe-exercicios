8b07b3de9111e71bc585831c891b1260bb168e6e
Malrus-dev <maelsobfer@gmail.com>
2022-04-28 23:59:42 -0300

    finalizando os bonus

--- trybe-exercicios/modulo-01-fundamentos/bloco-06-html-css-form-flexbox-responsivo/dia-01-html-css-forms/script.js
+++ trybe-exercicios/modulo-01-fundamentos/bloco-06-html-css-form-flexbox-responsivo/dia-01-html-css-forms/script.js
@@ -8,6 +8,16 @@ function startPage(){
 }
 function postForm(event){
     event.preventDefault();
+    for(let i = 0; i < formsValues.length; i += 1){
+        if(i === 9){
+            if(formsValues[i].checked === false){
+                alert("É necessário autorizar a utilização da sua imagem na divulgação de concursos futuros!")
+            }else{
+                alert("Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.")
+            }
+        }
+    }
+
 }
 function clearForm(){
     for(let i = 0; i < formsValues.length; i += 1){
